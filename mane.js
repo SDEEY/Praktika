889b913820b8f75dde8226258960e2b8f9d3c87f
George <seeyouagainpesh@gmail.com>
2020-12-20 00:24:30 +0500

    Sozdal cikl

--- mane.js
+++ mane.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посомтрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
 	count: numberOfFilms,
@@ -10,13 +10,27 @@ const personalMovieDB = {
 	privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-	  b = prompt('На сколько оцените его?', ''),
-	  c = prompt('Один из последних просмотренных фильмов?', ''),
-	  d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++){
+	const a = prompt('Один из последних просмотренных фильмов?', ''),
+		  b = prompt('На сколько оцените его?', '');
 
+	if ( a !=null && b !=null && a !='' && b !='' && a.length < 50) {
+
+	personalMovieDB.movies[a] = b;
+	} else {
+		i--;
+	}
+}
+
+if (personalMovieDB.count < 10) {
+	alert('Просмотрено довольно мало фильмов');
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+	alert('Вы классический зритель!');
+} else if(personalMovieDB.count > 30){
+	alert('Вы киноман!');
+} else{
+	alert('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
